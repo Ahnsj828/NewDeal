3f02917f97f89308ff65404d546e526c0fe222df
Ahnsj828 <ala828@naver.com>
2024-07-05 20:10:07 +0900

    240705

--- 07_July/0705/template-scss-basic/resources/js/main.js
+++ 07_July/0705/template-scss-basic/resources/js/main.js
@@ -1,13 +1,13 @@
- var swiper = new Swiper('.mySwiper', {
-                    slidesPerView: 1,
-                    spaceBetween: 0,
-                    loop: false,
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable: true,
-                    },
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
-                })
+var swiper = new Swiper(".slide-intro", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
