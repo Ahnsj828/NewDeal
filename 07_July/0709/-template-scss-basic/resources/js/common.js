e8170c9523423c4ae0fa43f143244b9f7c3d1490
Ahnsj828 <ala828@naver.com>
2024-07-09 09:28:05 +0900

    240709

---
+++ 07_July/0709/-template-scss-basic/resources/js/common.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here
+  const dl = document.querySelector("#gnb dl");
+  const dd = document.querySelector("#gnb dd");
+
+  // dl 마우스 오버시
+  dl.addEventListener("mouseover", function () {
+    dl.classList.add("active");
+  });
+
+  // dl 마우스 아웃시
+  dl.addEventListener("mouseout", function () {
+    dl.classList.remove("active");
+  });
+});
